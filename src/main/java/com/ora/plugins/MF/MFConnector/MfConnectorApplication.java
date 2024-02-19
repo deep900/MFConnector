d911a20d9006f65f38223cb859e942632158e443
Pradheep <deep90@gmail.com>
2024-02-19 11:40:20 +0800

    MF Connector plugin

---
+++ src/main/java/com/ora/plugins/MF/MFConnector/MfConnectorApplication.java
@@ -0,0 +1,13 @@
+package com.ora.plugins.MF.MFConnector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MfConnectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MfConnectorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ora/plugins/MF/MFConnector/MfConnectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ora.plugins.MF.MFConnector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MfConnectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
